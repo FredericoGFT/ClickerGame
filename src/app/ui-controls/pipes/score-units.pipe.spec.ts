7e14129e20c5d9f929ef3867552b6f48c45f5f99
FredericoGFT <fozg@gft.com>
2022-11-23 18:02:11 +0100

    ScoreUnitsPipe tests

--- src/app/ui-controls/pipes/score-units.pipe.spec.ts
+++ src/app/ui-controls/pipes/score-units.pipe.spec.ts
@@ -5,4 +5,34 @@ describe('ScoreUnitsPipe', () => {
     const pipe = new ScoreUnitsPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('k indicator should appear', () => {
+    const pipe = new ScoreUnitsPipe();
+    const result = pipe.transform(1000)
+    expect(result.indexOf('k')).toBeGreaterThan(5);
+  });
+
+  it('m indicator should appear', () => {
+    const pipe = new ScoreUnitsPipe();
+    const result = pipe.transform(1000000)
+    expect(result.indexOf('m')).toBeGreaterThan(8);
+  });
+
+  it('t indicator should appear', () => {
+    const pipe = new ScoreUnitsPipe();
+    const result = pipe.transform(1000000000)
+    expect(result.indexOf('t')).toBeGreaterThan(12);
+  });
+
+  it('g indicator should appear', () => {
+    const pipe = new ScoreUnitsPipe();
+    const result = pipe.transform(1000000000000)
+    expect(result.indexOf('g')).toBeGreaterThan(15);
+  });
+
+  it('p indicator should appear', () => {
+    const pipe = new ScoreUnitsPipe();
+    const result = pipe.transform(1000000000000000)
+    expect(result.indexOf('p')).toBeGreaterThan(18);
+  });
 });
